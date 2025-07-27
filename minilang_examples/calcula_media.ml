d1dbdc1529d01f2f402381eb916c6fac14d85a34
Estevao <estevaopfon@gmail.com>
2025-07-27 16:50:27 -0300

    fix(calcula_media.ml): change global variable declaration to let for proper scoping fix(calcula_media.ml): update while loop condition to use index instead of length for correct iteration refactor(calcula_media.ml): split print statement for better readability and clarity in output

--- minilang_examples/calcula_media.ml
+++ minilang_examples/calcula_media.ml
@@ -1,17 +1,16 @@
-global notas: int[5] = [10, 8, 7, 9, 6]
+let notas: int[5] = [10, 8, 7, 9, 6]
 let tamanho: int = length(notas)
 
 func calcula_media(notas: int[], tamanho: int) -> int
-    let len: int = tamanho
     let soma: int = 0
     let i: int = 0
 
-    while len >= 0 do
+    while i < tamanho do
         soma = soma + notas[i]
         i = i + 1
-        len = len - 1
     end
     return soma / tamanho
 end
 
-print("A média das notas é: " + to_str(calcula_media(notas, tamanho)))
+print("A média das notas é: ")
+print(calcula_media(notas, tamanho))
