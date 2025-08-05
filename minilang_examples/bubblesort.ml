9d95e3e7bb7846a8b64686cf0e8735ea710b9c11
Estevao <estevaopfon@gmail.com>
2025-08-04 21:31:50 -0300

    feat(bubblesort.ml): implement bubblesort function and refactor sorting logic into reusable functions feat(bubblesort.ml): add bubblesort_ref function to return sorted array feat(bubblesort.ml): create print_ref function to print array elements

--- minilang_examples/bubblesort.ml
+++ minilang_examples/bubblesort.ml
@@ -1,22 +1,53 @@
 
-let desordenado:bool = true
-let array: int[6] = [5, 3, 8, 6, 2, 7]
-
-while desordenado do
-    desordenado = false
-    let i: int = 0
-    while i < 5 do
-        if array[i] > array[i + 1] then
-          let temp: int = 0
-          temp = array[i]
-          array[i] = array[i + 1]
-          array[i + 1] = temp
-          desordenado = true
+func bubblesort(array: int[], tamanho: int) -> void
+    let desordenado: bool = true
+    while desordenado do
+        desordenado = false
+        let i: int = 0
+        while i < (tamanho - 1) do
+            if array[i] > array[i + 1] then
+                let temp: int = 0
+                temp = array[i]
+                array[i] = array[i + 1]
+                array[i + 1] = temp
+                desordenado = true
+            end
+            i = i + 1
         end
-    i = i + 1
     end
 end
-print("Ordenado")
 
-print(to_str(array))
-    
+let arr: int[6] = [5, 3, 8, 6, 2, 7]
+bubblesort(arr, 6)
+print(to_str(arr))
+
+func bubblesort_ref(array: int[], tamanho: int) -> int[]
+    let desordenado: bool = true
+    while desordenado do
+        desordenado = false
+        let i: int = 0
+        while i < (tamanho - 1) do
+            if array[i] > array[i + 1] then
+                let temp: int = 0
+                temp = array[i]
+                array[i] = array[i + 1]
+                array[i + 1] = temp
+                desordenado = true
+            end
+            i = i + 1
+        end
+    end
+    return array
+end
+
+let arr_ref: int[6] = [23, 6, 32, 64, 2, 7]
+
+func print_ref(array: int[], tamanho: int) -> void
+    let i: int = 0
+    while i < tamanho  do
+        print(to_str(array[i]) + " ")
+        i = i + 1
+    end
+end
+
+print_ref(bubblesort_ref(arr_ref, 6),6)
